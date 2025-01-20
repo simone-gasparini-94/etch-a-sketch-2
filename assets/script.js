84362df62fea2516d9e65e36308cbc3e327aecf4
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-20 11:57:11 +0100

    Adjust grid with fixed width and height
    Now the grid has a fixed width and height and the squares
    adjust accordingly

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
 const grid = document.querySelector(".grid");
 
+const gridSize = {
+    width: 960,
+    height: 960,
+}
+
 function createSquares(number) {
     for(i = 0; i < number; i++) {
         const row = document.createElement("div");
@@ -8,6 +13,12 @@ function createSquares(number) {
         for (j = 0; j < number; j++) {
             const square = document.createElement("div");
             square.setAttribute("class", "square");
+            const squareSize = {
+                width: gridSize.width / number + "px",
+                height: gridSize.height / number + "px",
+            }
+            square.style.width = squareSize.width;
+            square.style.height = squareSize.height;
             square.addEventListener("mouseover", changeColor);
             row.appendChild(square);
         }
