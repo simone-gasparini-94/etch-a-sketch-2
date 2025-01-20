518866188b9bef4ea87fa20f3f8a82ea647885ef
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-20 12:59:46 +0100

    Add button with event listener to select number of squares

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,31 @@
 const grid = document.querySelector(".grid");
+const createBtn = document.querySelector(".create-btn");
+const input = document.querySelector("#input-size");
 
 const gridSize = {
     width: parseFloat(getComputedStyle(grid).width),
     height: parseFloat(getComputedStyle(grid).height),
 }
 
-function createSquares(number) {
-    for(i = 0; i < number; i++) {
+function createSquares() {
+    grid.innerHTML = "";
+
+    let inputValue = parseInt(input.value);
+
+    if(isNaN(inputValue) || inputValue < 2 || inputValue > 64) {
+        inputValue = 16;
+    }
+
+    for(let i = 0; i < inputValue; i++) {
         const row = document.createElement("div");
         row.setAttribute("class", "row");
         grid.appendChild(row);
-        for (j = 0; j < number; j++) {
+        for (let j = 0; j < inputValue; j++) {
             const square = document.createElement("div");
             square.setAttribute("class", "square");
             const squareSize = {
-                width: gridSize.width / number + "px",
-                height: gridSize.height / number + "px",
+                width: gridSize.width / inputValue + "px",
+                height: gridSize.height / inputValue + "px",
             }
             square.style.width = squareSize.width;
             square.style.height = squareSize.height;
@@ -29,5 +39,6 @@ function changeColor(event) {
     event.target.style.backgroundColor = "red";
 }
 
+createBtn.addEventListener("click", createSquares);
 
-createSquares(16);
+createSquares();
